27ff63cb421a6d95746dbaed7af0d192bfb67667
cocteau666 <cocteau666@gmail.com>
2017-12-01 17:35:29 +0900

    add method device bound notification

--- FaAzureIoTHttpRuntime.lua
+++ FaAzureIoTHttpRuntime.lua
@@ -2,6 +2,18 @@ module("httpRuntime",package.seeeall)
 
 -- Http Runtime Api
 
+-- Receive Device Bound Notification
+function receiveDeviceBoundNotification(fa, iotName, id, auth)
+    local b,c,h = fa.request{url="https://"..iotName..".azure-devices.net/devices/"..id.."/messages/deviceBound?api-version=2016-11-14",
+        method = "GET",
+        headers = {["Authorization"]  = auth,
+                   ["Content-Length"] = body:len(),
+                   ["Content-Type"]   = "application/json"
+        }
+    }
+    return b
+end
+
 -- Send Device Event
 function sendDeviceEvent(fa, iotName, id, auth, body)
     local b,c,h = fa.request{url="https://"..iotName..".azure-devices.net/devices/"..id.."/messages/events?api-version=2016-11-14",
